f56cd99eb2136a769609060ce1f90b3511d54818
Błażej Bałęczny <blazej.baleczny@q-workshop.com>
2021-09-09 11:27:44 +0200

    Initial commit

---
+++ dragons-bottle-green-gold.js
@@ -0,0 +1,51 @@
+Hooks.once('diceSoNiceReady', (dice3d) => {
+	dice3d.addSystem({id: "dragons-bottle-green-gold", name: "Q WORKSHOP Dragons Bottle-green & Gold"}, false);
+	dice3d.addDicePreset({
+		type: "d2",
+		labels: "",
+		modelFile: "modules/dragons-bottle-green-gold/d2_dragons.glb",
+		system: "dragons-bottle-green-gold"
+	});
+	dice3d.addDicePreset({
+		type: "d4",
+		labels: "",
+		modelFile: "modules/dragons-bottle-green-gold/d4_dragons_SDRA98.glb",
+		system: "dragons-bottle-green-gold"
+	});
+	dice3d.addDicePreset({
+		type: "d6",
+		labels: "",
+		modelFile: "modules/dragons-bottle-green-gold/d6_dragons_SDRA98.glb",
+		system: "dragons-bottle-green-gold"
+	});
+	dice3d.addDicePreset({
+		type: "d8",
+		labels: "",
+		modelFile: "modules/dragons-bottle-green-gold/d8_dragons_SDRA98.glb",
+		system: "dragons-bottle-green-gold"
+	});
+	dice3d.addDicePreset({
+		type: "d10",
+		labels: "",
+		modelFile: "modules/dragons-bottle-green-gold/d10_dragons_SDRA98.glb",
+		system: "dragons-bottle-green-gold"
+	});
+	dice3d.addDicePreset({
+		type: "d100",
+		labels: "",
+		modelFile: "modules/dragons-bottle-green-gold/d100_dragons_SDRA98.glb",
+		system: "dragons-bottle-green-gold"
+	});
+	dice3d.addDicePreset({
+		type: "d12",
+		labels: "",
+		modelFile: "modules/dragons-bottle-green-gold/d12_dragons_SDRA98.glb",
+		system: "dragons-bottle-green-gold"
+	});
+	dice3d.addDicePreset({
+		type: "d20",
+		labels: "",
+		modelFile: "modules/dragons-bottle-green-gold/d20_dragons_SDRA98.glb",
+		system: "dragons-bottle-green-gold"
+	});
+});
